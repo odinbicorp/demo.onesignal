8816757a2a1849d71de6ac7060e9ca0a973f1f83
duynguyen <duyit192@hotmail.com>
2022-05-20 16:16:30 +0700

    add onesignal sw

--- resources/views/subscribe.blade.php
+++ resources/views/subscribe.blade.php
@@ -7,6 +7,15 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+  <script>
+    window.OneSignal = window.OneSignal || [];
+    OneSignal.push(function() {
+      OneSignal.init({
+        appId: "945ed348-3c59-4af9-b369-ceecb898fb68",
+      });
+    });
+  </script>
 </head>
 <body>
 
