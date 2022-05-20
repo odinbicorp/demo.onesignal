92fa9b3cb58215a0ead9ee8623ef2a02ae346642
duynguyen <duyit192@hotmail.com>
2022-05-20 17:27:17 +0700

    change app key onesignal

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -62,6 +62,7 @@ class NotificationController extends Controller
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8'));
         // curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
         //     'Authorization: Basic NGM0Mzk5YjMtNTQzMi00OTkwLTkyY2EtMTI1MzNhODBmZjgz'));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
